81c73b5c49e316578503f5a9bc70c8b4ad724d41
henrikhestnes <hahestne@stud.ntnu.no>
2021-02-28 20:21:28 +0100

    small changes on orders module

---
+++ elevator_project/lib/elevator.ex
@@ -0,0 +1,4 @@
+defmodule Elevator do
+  use GenServer
+  
+end
--- elevator_project/lib/orders.ex
+++ elevator_project/lib/orders.ex
@@ -1,22 +1,23 @@
 defmodule Orders do
   use Agent
 
-  @valid_order [:hall_down, :cab, :hall_up]
+  @valid_order [:cab, :hall_down, :hall_up]
 
-  def start_link() do
-    init_map = %{:cab => [], :hall}
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  def start_link do
+    Agent.start_link(fn -> %{:cab => [], :hall_down => [], :hall_up => []} end, name: __MODULE__)
   end
 
-  def set_order(order) do
-    Agent.update(__MODULE__, fn map -> Map.put(map, order, :true) end)
+  def new(floor, button_type) when is_integer(floor) and button_type in @valid_order do
+    Agent.update(__MODULE__,
+    fn map -> Map.update(map, button_type, [], fn list -> Enum.uniq([floor | list]) end) end)
   end
 
-  def clear_order(order) do
-    Agent.update(__MODULE__, fn map -> Map.delete(map, order) end)
+  def delete(floor, button_type) when is_integer(floor) and button_type in @valid_order do
+    Agent.update(__MODULE__, fn map -> Map.update(map, button_type, [], fn list -> List.delete(list, floor) end) end)
   end
 
-  def get_orders do
+  def get do
     Agent.get(__MODULE__, fn orders -> orders end)
   end
+  
 end
