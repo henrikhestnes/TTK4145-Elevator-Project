7b04bb36e902d6422889b4e3e111dc57b41b0839
Endre Bruaset <endre.bruaset@gmail.com>
2021-02-28 22:50:04 +0100

    Added implementation of direction choice and stopping logic to order module

--- elevator_project/lib/elevator.ex
+++ elevator_project/lib/elevator.ex
@@ -1,4 +1,19 @@
 defmodule Elevator do
   use GenServer
-  
+
+  @n_floors 4
+
+  defstruct [:floor, :direction, :fsm_state]
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  # API
+
+
+  # Casts
+
+  # Calls
+
 end
--- elevator_project/lib/orders.ex
+++ elevator_project/lib/orders.ex
@@ -3,6 +3,7 @@ defmodule Orders do
 
   @valid_order [:cab, :hall_down, :hall_up]
 
+  # API
   def start_link do
     Agent.start_link(fn -> %{:cab => [], :hall_down => [], :hall_up => []} end, name: __MODULE__)
   end
@@ -16,8 +17,58 @@ defmodule Orders do
     Agent.update(__MODULE__, fn map -> Map.update(map, button_type, [], fn list -> List.delete(list, floor) end) end)
   end
 
-  def get do
+  def choose_direction(%Elevator{} = e) do
+    case e.direction do
+      :up ->
+        cond do
+          orders_above?(e.floor) -> :up
+          orders_below?(e.floor) -> :down
+          true -> :stop
+        end
+
+      _->
+        cond do
+          orders_below?(e.floor) -> :down
+          orders_above?(e.floor) -> :up
+          true -> :stop
+        end
+    end
+  end
+
+  def should_stop?(%Elevator{} = e) do
+    case e.direction do
+      :up ->
+        e.floor in Map.get(order_map(), :cab) or
+        e.floor in Map.get(order_map(), :hall_up) or
+        !orders_above?(e.floor)
+      :down ->
+        e.floor in Map.get(order_map(), :cab) or
+        e.floor in Map.get(order_map(), :hall_down) or
+        !orders_below?(e.floor)
+      _->
+        true
+    end
+  end
+
+  # Private helper functions
+  def order_map do
     Agent.get(__MODULE__, fn orders -> orders end)
   end
-  
+
+  def orders_above?(floor) do
+    Map.values(order_map())
+    |> List.flatten()
+    |> Enum.filter(fn v -> v > floor end)
+    |> Enum.any?
+  end
+
+
+  def orders_below?(floor) do
+    Map.values(order_map())
+    |> List.flatten()
+    |> Enum.filter(fn v -> v < floor end)
+    |> Enum.any?
+  end
+
+
 end
