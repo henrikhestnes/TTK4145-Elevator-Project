a9a88eb9d0a8aa7575c3ddfdcfdea29bd43fa448
henrikhestnes <hahestne@stud.ntnu.no>
2021-03-24 17:54:02 +0100

    updated OrderAssigner

--- elevator_project/lib/order_assigner.ex
+++ elevator_project/lib/order_assigner.ex
@@ -7,34 +7,40 @@ defmodule OrderAssigner do
   end
   # API ------------------------------------------------
   def new_order(%Order{} = order) do
-    case Map.get(order, :button_type) do
-      :cab -> GenServer.cast(__MODULE__, {:new_cab_order, order})
-      _    -> GenServer.cast(__MODULE__, {:new_hall_order, order})
-    end
+    case order.button_type do
+      :cab -> GenServer.cast(@name, {:new_cab_order,  %Order{} = order})
+      _    -> GenServer.cast(@name, {:new_hall_order, %Order{} = order})
+      end
   end
 
-  # Casts -----------------------------------------------new_hall_order
+  # Init ------------------------------------------------
+  # def init({init_arg}) do
+  #   {:ok, init_arg}
+  # end
+
+  # Casts -----------------------------------------------
+  @impl true
   def handle_cast({:new_hall_order, %Order{} = order}, _state) do
     all_nodes = [Node.self() | Node.list()]
-    IO.puts("HALL")
-    ##ask for other nodes cost
-    GenServer.multi_call(all_nodes, @name, )
-    ##Choose elevator, send to OrderDistributor
-    {:noreply, _state}
+    {replies, _bad_nodes} = GenServer.multi_call(all_nodes, @name, {:get_cost, order})
+    best_elevator = replies |> List.keysort(1)
+                            |> List.first()
+                            |> elem(0)
+    OrderDistributor.handle_order(order, best_elevator)
+    {:noreply}
   end
 
+  @impl true
   def handle_cast({:new_cab_order, %Order{} = order}, _state) do
-    IO.puts("CAB")
-    #Samkjør med OrderDistributor
-    #OrderDistributor.handle_order()
-    {:noreply, _state}
+    OrderDistributor.handle_order(order, Node.self())
+    {:noreply}
   end
 
   # Calls -----------------------------------------------
   def handle_call({:get_cost, %Order{} = order}, _state) do
     ##calculate cost
     cost = OrderAssigner.CostCalculation.cost(order)
-    {:reply, {%Order{} = order, cost}, _state}
+    {:reply, {order, cost}}
   end
 
   # Helper functions ------------------------------------
@@ -44,7 +50,7 @@ defmodule OrderAssigner.CostCalculation do
   def cost(%Order{} = order) do
     ## Implement cost function
     ## get order map from elevator_operator
-    Elevator.get_orders();
-    cost = 1;
+    orders = Elevator.get_orders();
+    #cost = 1;
   end
 end
