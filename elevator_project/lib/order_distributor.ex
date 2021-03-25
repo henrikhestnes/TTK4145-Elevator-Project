c735d802add2410bcedb8f8efd8e0a9248adcc26
henrikhestnes <hahestne@stud.ntnu.no>
2021-03-25 16:28:57 +0100

    updates in order_assigner

--- elevator_project/lib/order_distributor.ex
+++ elevator_project/lib/order_distributor.ex
@@ -8,13 +8,13 @@ defmodule OrderDistributor do
   end
 
   def add_order(order = %Order{}, node) do
-    GenServer.call(node, {:add_order, order}) 
-     
+    GenServer.call(node, {:add_order, order})
+
   end
 
   def delete_order(%Order{}, node) do
-    GenServer.call(node, {:delete_order, order}) 
-      
+    #GenServer.call(node, {:delete_order, order})
+
   end
 
   def broadcast(order = %Order{}) do
@@ -38,8 +38,8 @@ defmodule OrderDistributor do
   end
 
   def handle_cast({:request_backup}, _from, state) do
-    Genserver.cast(__MODULE__, {:send_backup, from})
-  end 
+    #Genserver.cast(__MODULE__, {:send_backup, from})
+  end
 
   def handle_call({:add_order, order}, _from, state) do
     Orders.new(order.button_type, order.floor)
@@ -47,7 +47,7 @@ defmodule OrderDistributor do
   end
 
   def handle_call({:delete_order, order}) do
-    
+
   end
 
 end
