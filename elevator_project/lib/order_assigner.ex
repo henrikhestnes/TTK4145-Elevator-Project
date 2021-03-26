271a2ad8053f2847fbd4f2db767741a65ff64a8e
henrikhestnes <hahestne@stud.ntnu.no>
2021-03-26 18:30:44 +0100

    calling for cost almost working, weird multi_call bug

--- elevator_project/lib/order_assigner.ex
+++ elevator_project/lib/order_assigner.ex
@@ -1,5 +1,6 @@
 defmodule OrderAssigner do
   use GenServer
+  alias OrderAssigner.CostCalculation
   @name :order_assigner
   @auction_timeout 1_000
 
@@ -18,45 +19,51 @@ defmodule OrderAssigner do
   end
 
   # Init ------------------------------------------------
-  # @impl true
-  # def init(init_arg) do
-  #   {:ok, init_arg}
-  # end
+  def init(_init_arg) do
+    {:ok, []}
+  end
 
   # Casts -----------------------------------------------
   @impl true
   def handle_cast({:new_hall_order, %Order{} = order}, state) do
-    #IO.puts("hello")
-    all_nodes = [Node.self() | Node.list()]
-    #{replies, _bad_nodes} = GenServer.multi_call(all_nodes, @name, {:get_cost, order}, @auction_timeout)
-    #best_elevator = replies |> List.keysort(1) |> List.first() |> elem(0)
-    IO.puts("helo")
+    all_nodes = [self() | Node.list]
+    {replies, _bad_nodes} = GenServer.multi_call(all_nodes, @name, {:get_cost, order}, @auction_timeout)
+    best_elevator =
+    if Enum.any?(replies) do
+      replies |> List.keysort(1) |> List.first() |> elem(0)
+    else
+      :no_response
+    end
+    IO.puts(best_elevator)
     #OrderDistributor.handle_order(order, best_elevator)
     {:noreply, state}
   end
 
   @impl true
-  def handle_cast({:new_cab_order, %Order{} = order}, state) do
+  def handle_cast({:new_cab_order, %Order{} = _order}, state) do
     #OrderDistributor.handle_order(order, Node.self())
-    IO.puts("yessir")
     {:noreply, state}
   end
 
   # Calls -----------------------------------------------
-  def handle_call({:get_cost, %Order{} = order}, _state) do
+  @impl true
+  def handle_call({:get_cost, %Order{} = _order}, _from, state) do
     ##calculate cost
-    cost = OrderAssigner.CostCalculation.cost(order)
-    {:reply, {order, cost}}
+    IO.puts("Calculating cost")
+    cost = CostCalculation.cost()
+    {:reply, cost, state}
   end
 
   # Helper functions ------------------------------------
 end
 
 defmodule OrderAssigner.CostCalculation do
-  def cost(%Order{} = order) do
-    ## Implement cost function
-    ## get order map from elevator_operator
-    orders = Elevator.get_orders();
-    #cost = 1;
+  def cost() do
+    _num_orders =
+      Elevator.Orders.get()
+      |> Map.values()
+      |> List.flatten()
+      |> length()
+
   end
 end
--- elevator_project/lib/tests.ex
+++ elevator_project/lib/tests.ex
@@ -1,12 +1,12 @@
 defmodule Test do
   def merge do
-    order_backup1 = [ %Order{button_type: :cab, floor: 2}, 
+    order_backup1 = [ %Order{button_type: :cab, floor: 2},
                       %Order{button_type: :hall_down, floor: 2},
                       %Order{button_type: :cab, floor: 3}]
-    order_backup2 = [ %Order{button_type: :cab, floor: 1}, 
+    order_backup2 = [ %Order{button_type: :cab, floor: 1},
                       %Order{button_type: :hall_down, floor: 2},
                       %Order{button_type: :cab, floor: 4}]
-    order_backup3 = [ %Order{button_type: :hall_up, floor: 2}, 
+    order_backup3 = [ %Order{button_type: :hall_up, floor: 2},
                       %Order{button_type: :hall_down, floor: 2},
                       %Order{button_type: :cab, floor: 3}]
     OrderBackup.merge([order_backup1, order_backup2, order_backup3])
@@ -14,7 +14,15 @@ defmodule Test do
   end
 
   def remove_order() do
-    
+
+  end
+
+  def order_assigner_broadcast() do
+    #Network.Init.start_node("henrik")
+    #OrderAssigner.start_link()
+    #Elevator.Orders.start_link()
+    Elevator.Orders.new(:hall_up, 2)
+    order = %Order{button_type: :hall_up, floor: 1, watchdog_ref: nil}
+    OrderAssigner.new_order(order)
   end
 end
-
