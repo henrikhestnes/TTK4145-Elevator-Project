59841e37ae5698691948020b2cef2f2295c042cf
Endre Bruaset <endre.bruaset@gmail.com>
2021-04-08 21:22:51 +0200

    Tested floor poller

--- elevator_project/lib/input_poller.ex
+++ elevator_project/lib/input_poller.ex
@@ -31,12 +31,6 @@ defmodule FloorPoller do
 
   def poller(prev_state) do
     current_state = Driver.get_floor_sensor_state()
-    # case {prev_state, current_state} do
-    #   {:between_floors, floor} when floor != :between_floors ->
-    #     Elevator.floor_arrival(floor)
-    #   _ -> :ok
-    # end
-
     if current_state != :between_floors and prev_state == :between_floors do
       Elevator.floor_arrival(current_state)
     end
