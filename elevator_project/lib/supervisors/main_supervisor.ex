5d86cfcd4e554eba695b6bf86301885b195bfc2f
Endre Bruaset <endre.bruaset@gmail.com>
2021-04-14 08:32:19 +0200

    Prepared running application with mix run

--- elevator_project/lib/supervisors/main_supervisor.ex
+++ elevator_project/lib/supervisors/main_supervisor.ex
@@ -4,35 +4,35 @@ defmodule ElevatorProject.Supervisor do
 
   use Supervisor
 
-  def start_link(node_name, driver_port) do
-    Supervisor.start_link(__MODULE__, [node_name, driver_port], name: __MODULE__)
-  end
-
-  @impl true
-  def init([node_name, driver_port]) do
-    children = [
-      {HardwareSupervisor, [@number_of_floors, driver_port]},
-      {Network.Supervisor, node_name},
-      OrderDistributor.Supervisor,
-      OrderAssigner.Supervisor,
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
-  end
-
-  # def start_link([]) do
-  #   Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  # def start_link(node_name, driver_port) do
+  #   Supervisor.start_link(__MODULE__, [node_name, driver_port], name: __MODULE__)
   # end
 
   # @impl true
-  # def init([]) do
+  # def init([node_name, driver_port]) do
   #   children = [
-  #     {HardwareSupervisor, @number_of_floors},
-  #     {Network.Supervisor, @node_name},
+  #     {HardwareSupervisor, [@number_of_floors, driver_port]},
+  #     {Network.Supervisor, node_name},
   #     OrderDistributor.Supervisor,
   #     OrderAssigner.Supervisor,
   #   ]
 
   #   Supervisor.init(children, strategy: :one_for_one)
   # end
+
+  def start_link([]) do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true
+  def init([]) do
+    children = [
+      {HardwareSupervisor, @number_of_floors},
+      {Network.Supervisor, @node_name},
+      OrderDistributor.Supervisor,
+      OrderAssigner.Supervisor,
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 end
