82ca44595c217441465b63f8bba53b3225f81ace
LasseWardenaer <lassetw@stud.ntnu.no>
2021-04-17 12:14:56 +0200

    Added documentation template

--- elevator_project/mix.exs
+++ elevator_project/mix.exs
@@ -7,7 +7,14 @@ defmodule ElevatorProject.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      name: "Elevator project",
+      source_url: "https://github.com/TTK4145-Students-2021/project-gruppe-26",
+      docs: [
+        main: "Elevator project",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +30,7 @@ defmodule ElevatorProject.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:gen_state_machine, "~> 3.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
