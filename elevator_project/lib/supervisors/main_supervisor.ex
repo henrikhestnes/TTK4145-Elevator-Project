3512cb604a62a78bc98014638125dba9bdc7d74e
Endre Bruaset <endre.bruaset@gmail.com>
2021-04-21 08:44:34 +0200

    Prepared for running simulator with mix run at FAT test

--- elevator_project/lib/supervisors/main_supervisor.ex
+++ elevator_project/lib/supervisors/main_supervisor.ex
@@ -7,36 +7,39 @@ defmodule ElevatorProject.Supervisor do
   @node_name "heis"
 
 ## FOR RUNNING THE PHYSICAL ELEVATOR
-  def start_link([]) do
-    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
-  end
-
-  @impl true
-  def init([]) do
-    children = [
-      {Network.Supervisor, @node_name},
-      OrderDistributor.Supervisor,
-      OrderAssigner.Supervisor,
-      {HardwareSupervisor, @number_of_floors}
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
-  end
-
-## FOR RUNNING THE SIMULATOR
-  # def start_link(node_name, driver_port) do
-  #   Supervisor.start_link(__MODULE__, [node_name, driver_port], name: __MODULE__)
+  # def start_link([]) do
+  #   Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   # end
 
   # @impl true
-  # def init([node_name, driver_port]) do
+  # def init([]) do
   #   children = [
-  #     {Network.Supervisor, node_name},
+  #     {Network.Supervisor, @node_name},
   #     OrderDistributor.Supervisor,
   #     OrderAssigner.Supervisor,
-  #     {HardwareSupervisor, [@number_of_floors, driver_port]}
+  #     {HardwareSupervisor, @number_of_floors}
   #   ]
 
   #   Supervisor.init(children, strategy: :one_for_one)
   # end
+
+## FOR RUNNING THE SIMULATOR
+  def start_link([]) do
+    start_link(@node_name, 15657)
+  end
+  def start_link(node_name, driver_port) do
+    Supervisor.start_link(__MODULE__, [node_name, driver_port], name: __MODULE__)
+  end
+
+  @impl true
+  def init([node_name, driver_port]) do
+    children = [
+      {Network.Supervisor, node_name},
+      OrderDistributor.Supervisor,
+      OrderAssigner.Supervisor,
+      {HardwareSupervisor, [@number_of_floors, driver_port]}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 end
